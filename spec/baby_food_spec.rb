752d1dc9b0deafc8772914c92cf07c889516fc51
Gillian Wenhold <gwenhold@mac.com>
2019-08-05 12:45:47 -0400

    Done.

--- spec/baby_food_spec.rb
+++ spec/baby_food_spec.rb
@@ -1,19 +1,37 @@
 require "spec_helper"
 
 RSpec.describe BabyFood do
-  # Move your tests into logical groupings with context
+  describe "#favorite_kinds" do
+    it "puts 'wwwwWWWWWAAAHHHHH!' for small banana slices" do
+      expect { BabyFood.new(kind: "banana slices", size: "small") }.to output("wwwwWWWWWAAAHHHHH!\n").to_stdout
+    end
 
-  it "puts 'num nums' for small peas" do
-    expect { BabyFood.new(kind: "peas", size: "small") }.to output("num nums\n").to_stdout
+    it "puts 'wwwwWWWWWAAAHHHHH!' for large banana slices" do
+      expect { BabyFood.new(kind: "banana slices", size: "large") }.to output("wwwwWWWWWAAAHHHHH!\n").to_stdout
+    end
+
+    it "puts 'num nums' when the baby food kind is a FAVORITE_KIND" do
+      FAVORITE_KINDS = [
+        "peas",
+        "carrots",
+        "apple sauce",
+        "peach"
+      ]
+      FAVORITE_KINDS.each do |f|
+        expect { BabyFood.new(kind: f, size: "small") }.to output("num nums\n").to_stdout
+      end
+    end
   end
 
-  # These tests are pending until they are passed a block. Follow the formula for 'small peas' to make these work.
-  it "puts 'WAAAAHHH!' for large peas"
-  it "puts 'wwwwWWWWWAAAHHHHH!' for small banana slices"
-  it "puts 'num nums' small carrots"
-  it "puts 'Whhhaaaa WAAAAHHHHH!!' for hard small peas"
+  describe "#size" do
+    it "puts 'WAAAAHHH!' for large peas" do
+      expect { BabyFood.new(kind: "peas", size: "large") }.to output("WAAAAHHH!\n").to_stdout
+    end
+  end
 
-  # Can you write one test using iteration to check all the FAVORITE_KINDS of baby food?
-
-  # This spec file is complete when all favorite foods, invalid size, and invalid texture scenarios have been tested.
+  describe "#texture" do
+    it "puts 'Whhhaaaa WAAAAHHHHH!!' for hard small peas" do
+      expect { BabyFood.new(kind: "peas", size: "small", texture: "hard") }.to output("Whhhaaaa WAAAAHHHHH!!\n").to_stdout
+    end
+  end
 end
